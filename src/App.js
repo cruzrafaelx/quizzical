0b2cbcedea4dc5908d22c3039b8b4eb062481c73
cruzrafaelx <rafaelcruz@outlook.de>
2024-10-02 05:59:10 +0200

    create an array that holds the question and the answers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import React from 'react';
 import Landing from './Components/Landing';
-import Questions from './Components/Questions';
+import Quiz from './Components/Quiz';
 import {useState, useEffect} from 'react'
+import { nanoid } from 'nanoid';
+import {decode} from 'html-entities'
 
 function App() {
 
@@ -11,6 +13,11 @@ function App() {
 
   //questions state to store the retrieved questions
   const [questions, setQuestions] = useState([])
+
+   //Function to toggle start
+   const toggleStart = () => {
+    setStart(prevStart => !prevStart)
+  }
 
   //useEffect to handle API retrieval
   useEffect(() => {
@@ -38,21 +45,30 @@ function App() {
     },[])
 
 
-  //Function to toggle start
-  const toggleStart = () => {
-    setStart(prevStart => !prevStart)
-  }
+    //Array that holds the question in index 0 and answers arranged randomly 
+    const questionAndAnswer = questions.map(question => {
+      
+      //Array of decoded incorrect answers
+      let orderedArray = question.incorrect_answers.map(item => decode(item))
+  
+      const randomIndex = Math.floor(Math.random() * 4)
 
-  //Array that holds the questions which will be passed as a prop to Questions.js
-  const questionsArray = questions.map((question, index) =>{
-      return <p key={index}>{question.question}</p>
-  })
+      //Insert decoded correct answer with a random index using splice
+      orderedArray.splice(randomIndex, 0, decode(question.correct_answer))
+
+      //Manually inserting the question in index 0
+      return [question.question, ...orderedArray]
+    })
+
+
+  console.log(questionAndAnswer)
+  
 
   return (
     <div className="App">
       {!start
        ? <Landing start={toggleStart} /> 
-       : <Questions questions={questionsArray}/>}
+       : <Quiz quiz={questionAndAnswer}/>}
     </div>
   );
 }
