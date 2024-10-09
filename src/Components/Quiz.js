1cf43ffcd671b14c84ce4a8a7f80cc5c798dca02
cruzrafaelx <rafaelcruz@outlook.de>
2024-10-09 02:47:15 +0200

    modify color changes when chosen answers are correct or wrong

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -9,35 +9,47 @@ export default function Quiz({answers,
                               allAnswered}){
       
       return(
-      <section className="questions-container">
-            <div>
-                  {
-                  questions.map((question, index) =>(
-                  <div key={question.key} className="question-box">
-                        <p>{question.value}</p>
-                  
-                        <div className="answers-container"> 
-                              {
-                              answers[index] && answers[index].map(answer => (
-                                    <p key={answer.key} 
-                                    className={`answer 
-                                                ${answer.isHeld ? "isHeld": ""}
-                                                ${answer.wasNotChosen ? "not-chosen" : ""}`.trim()} 
-                                    onClick={()=>toggle(answer.key, index)}>{answer.value}</p>
-                              ))
-                              }
-                        </div>
+      <section className="questions-background">
+            <div className="questions-container">
+                  <div>
+                        {
+                        questions.map((question, index) =>(
+                        <div key={question.key} className="question-box">
+                              <p className="question">{question.value}</p>
+                        
+                              <div className="answers-container"> 
+                                    {
+                                    answers[index] && answers[index].map(answer => { 
+                                          const correct = answer.isHeld && answer.isCorrect
 
+                                          const wrong = answer.isHeld && !answer.isCorrect
+
+                                          return (
+                                          <p key={answer.key} 
+                                          className={`answer 
+                                                      ${answer.isHeld ? "isHeld": ""}
+                                                      ${submit && wrong ? "wrong" : ""}
+                                                      ${submit && correct ? "correct" : ""}
+                                                      ${submit && answer.isCorrect ? "correct" :""}
+                                                      ${submit ? "no-click" : ""}
+                                                      `.trim()} 
+                                          onClick={()=>toggle(answer.key, index)}>{answer.value}</p>
+                                    )})
+                                    }
+                              </div>
+
+                        </div>
+                        ))
+                        }
+                        {
+                              !submit ?
+                              <button onClick={()=>submitBtn()}
+                                    disabled={!allAnswered}>Submit</button> :
+                              <button onClick={()=>newGameBtn()}>New game</button>
+                        }
                   </div>
-                  ))
-                  }
-                  {
-                        !submit ?
-                        <button onClick={()=>submitBtn()}
-                                disabled={!allAnswered}>Submit</button> :
-                        <button onClick={()=>newGameBtn()}>New game</button>
-                  }
-            </div>
+            </div> 
+            
       </section>
       )
 }
